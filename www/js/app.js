713cbecf18d6a553fb166ed9e2d4218b908cd4d9
akaFTS <gustavo84171@hotmail.com>
2016-07-01 20:37:38 -0300

    Feita tela de exercicios

--- www/js/app.js
+++ www/js/app.js
@@ -140,5 +140,5 @@ app.config(function($stateProvider, $urlRouterProvider) {
     });
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/stats');
+    $urlRouterProvider.otherwise('/app/exers');
 });
